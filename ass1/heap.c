52229f26189bfbbd7cba6184f76ce9e9e3315820
root <crdewberry@crimson.ua.edu>
2018-02-11 16:39:00 -0600

    added in missing files. started heap.c

--- ass1/heap.c
+++ ass1/heap.c
@@ -1,22 +1,42 @@
     #include <stdio.h>
     #include <stdlib.h>
     #include <assert.h>
+    #include "stack.h"
+    #include "queue.h"
     #include "bst.h"
 
+
     typedef struct heap{
-        
+        int size;
+        BST *tree;
+        QUEUE *queue;
+        STACK *stack;
+        void (*display)(void *,FILE *);
+        int (*compare)(void *, void *);
+        void (*free)(void *);
         }HEAP;
 
 
 
     HEAP *newHEAP(
-        void (*)(void *,FILE *),    //display
-        int (*)(void *,void *),     //compare
-        void (*)(void *)){          //free
-        HEAP
+        void (*d)(void *,FILE *),    //display
+        int (*c)(void *,void *),     //compare
+        void (*f)(void *)){          //free
+        HEAP *heap = malloc(sizeof(HEAP));
+        heap->size = 0;
+        heap->tree = newBST(d,0,0,f);
+        heap->queue = newQUEUE(0,0);
+        heap->stack = newSTACK(0,0);
+        heap->display = d;
+        heap->compare = c;
+        heap->free = f;
+        return heap
         }
 
-    void insertHEAP(HEAP *h,void *value);
+    void insertHEAP(HEAP *h,void *value){
+        
+        }
+
     void buildHEAP(HEAP *h);
     void *peekHEAP(HEAP *h);
     void *extractHEAP(HEAP *h);
